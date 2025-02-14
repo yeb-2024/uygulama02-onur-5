5d2cf41362d61a58c294ae999d9b294684ee8e32
HP <seyfeddinonur055@gmail.com>
2025-02-14 18:11:39 +0300

    şifre ve kullanıcı adı kısmı yapıldı

--- untitled/src/giriş.java
+++ untitled/src/giriş.java
@@ -5,12 +5,14 @@ public class giriş {
 
     JFrame frame;
     JTextField kullanıcıadı;
+    JTextField şifre;
+
 
 
     public giriş(){
         JPanel panelüst    = new JPanel();
 
-        panelüst.setLayout(new FlowLayout()   );
+        panelüst.setLayout(new FlowLayout(FlowLayout.RIGHT)   );
         JLabel labelğst = new JLabel("kullanıcı adı");
         labelğst.setFont(new Font("Arial", Font.PLAIN, 20));
 
@@ -20,11 +22,26 @@ public class giriş {
         panelüst.add(labelğst);
         panelüst.add(kullanıcıadı);
 
+        JPanel panelalt    = new JPanel();
+
+        panelalt.setLayout(new FlowLayout(FlowLayout.RIGHT)   );
+        JLabel labelalt = new JLabel(" şifre");
+        labelalt.setFont(new Font("Arial", Font.PLAIN, 20));
+
+
+        şifre = new JTextField();
+        şifre.setPreferredSize(new Dimension(200,40));
+        panelalt.add(labelalt);
+        panelalt.add(şifre);
+
+
+
         frame= new JFrame("giriş");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
-        frame.setSize(400,300);
+        frame.setSize(500,400);
         frame.setLayout(new GridLayout(3,1));
         frame.add(panelüst);
+        frame.add(panelalt);
         frame.setVisible(true);
 
 
