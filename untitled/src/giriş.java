236fd54988a33e9fd6b54c5801ef659b2104bdb5
HP <seyfeddinonur055@gmail.com>
2025-02-14 18:05:12 +0300

    kullanıcı adı kısmı

--- untitled/src/giriş.java
+++ untitled/src/giriş.java
@@ -4,12 +4,28 @@ import java.awt.*;
 public class giriş {
 
     JFrame frame;
+    JTextField kullanıcıadı;
+
 
     public giriş(){
-        frame= new JFrame();
+        JPanel panelüst    = new JPanel();
+
+        panelüst.setLayout(new FlowLayout()   );
+        JLabel labelğst = new JLabel("kullanıcı adı");
+        labelğst.setFont(new Font("Arial", Font.PLAIN, 20));
+
+
+        kullanıcıadı = new JTextField();
+        kullanıcıadı.setPreferredSize(new Dimension(200,40));
+        panelüst.add(labelğst);
+        panelüst.add(kullanıcıadı);
+
+        frame= new JFrame("giriş");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
         frame.setSize(400,300);
         frame.setLayout(new GridLayout(3,1));
+        frame.add(panelüst);
         frame.setVisible(true);
+
 
     }}
