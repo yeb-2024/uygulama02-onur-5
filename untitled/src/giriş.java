43e0c3a62dc9480df03989abf7097296f94575dc
HP <seyfeddinonur055@gmail.com>
2025-02-14 17:57:55 +0300

    giriş

---
+++ untitled/src/giriş.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class giriş {
+
+    JFrame frame;
+
+    public giriş(){
+        frame= new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        frame.setSize(400,300);
+        frame.setLayout(new GridLayout(3,1));
+        frame.setVisible(true);
+
+    }}
